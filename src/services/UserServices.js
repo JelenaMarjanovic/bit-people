e3a3dce9df2ef67d8feca2964280b4bf7265b713
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2018-09-16 13:53:13 +0200

    Fix date of birth - changed API

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -3,15 +3,25 @@ import axios from 'axios';
 import User from '../models/User';
 
 class UserService {
-    getUsers() {
-        return axios.get('https://randomuser.me/api/?results=45')
-            .then((result) => {
-                return result.data.results;
-            })
-            .then((result) => {
-                return result.map(user => new User(user.picture, user.name, user.email, user.dob, user.gender));
-            })
-    }
+  getUsers() {
+    return axios
+      .get('https://randomuser.me/api/?results=45')
+      .then(result => {
+        return result.data.results;
+      })
+      .then(result => {
+        return result.map(
+          user =>
+            new User(
+              user.picture,
+              user.name,
+              user.email,
+              user.dob.date,
+              user.gender
+            )
+        );
+      });
+  }
 }
 
 export const userService = new UserService();
